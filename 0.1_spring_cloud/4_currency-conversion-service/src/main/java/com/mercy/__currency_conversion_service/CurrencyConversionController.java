4b62279b6e3273d511a262efc0309c3708fdd41a
Sasi Kumar <sasi.kumar@tavant.com>
2025-10-17 15:53:56 +0530

    Connecting currency-conversion service with currency-exchange service

--- 0.1_spring_cloud/4_currency-conversion-service/src/main/java/com/mercy/__currency_conversion_service/CurrencyConversionController.java
+++ 0.1_spring_cloud/4_currency-conversion-service/src/main/java/com/mercy/__currency_conversion_service/CurrencyConversionController.java
@@ -3,23 +3,48 @@ package com.mercy.__currency_conversion_service;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class CurrencyConversionController {
- 
+
 	@Autowired
-	private Environment env;
-	
-	@GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	private RestTemplate restTemplate;
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion convertCurrency(@PathVariable String from,
 			@PathVariable String to, @PathVariable BigDecimal quantity) {
-		
-		String port = env.getProperty("local.server.port");
-		
-		return new CurrencyConversion(200l, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, port);
+
+		String url = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
+
+		UriComponents uriComponents = UriComponentsBuilder
+				.fromUriString(url)
+				.buildAndExpand(from, to);
+
+		CurrencyConversion obj = restTemplate.getForObject(uriComponents.toUri(), CurrencyConversion.class);
+
+		return new CurrencyConversion(obj.getId(), 
+				obj.getFrom(), 
+				obj.getTo(), 
+				obj.getQuantity(), obj.getConversionMultiple(), 
+				quantity.multiply(obj.getConversionMultiple()), 
+				obj.getEnvironment());
+	}
+}
+
+@Configuration
+class AppConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
