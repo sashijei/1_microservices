739cf111b85f4127ee777a552f01c1423b5143a4
Sasi Kumar <sasi.kumar@tavant.com>
2025-10-29 19:57:01 +0530

    Adding routing for currency conversion service

--- 5_server_side_discovery_pattern_single_customer-service/api-gateway/src/main/java/com/mercy/api_gateway/ApiGatewayConfiguration.java
+++ 5_server_side_discovery_pattern_single_customer-service/api-gateway/src/main/java/com/mercy/api_gateway/ApiGatewayConfiguration.java
@@ -11,17 +11,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApiGatewayConfiguration {
+public class ApiGatewayConfiguration { 
 	
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		
-			Function<PredicateSpec, Buildable<Route>> currencyExchangeouteFunction 
+			// Defining the routing logic here
+			Function<PredicateSpec, Buildable<Route>> currencyExchangeRouteFunction 
 				= p -> p.path("/currency-exchange/**")
 								.uri("lb://currency-exchange");
 			 
+			Function<PredicateSpec, Buildable<Route>> currencyConversionRouteFunction 
+				= p -> p.path("/currency-conversion/**")
+								.uri("lb://currency-conversion");
+				
+			Function<PredicateSpec, Buildable<Route>> currencyConversionFeignRouteFunction 
+				= p -> p.path("/currency-conversion-feign/**")
+								.uri("lb://currency-conversion");
+				
 			return builder.routes()
-					.route(currencyExchangeouteFunction)
+					.route(currencyExchangeRouteFunction)
+					.route(currencyConversionRouteFunction)
+					.route(currencyConversionFeignRouteFunction)
 					.build();
 		
 	}
